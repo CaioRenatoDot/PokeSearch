c3a5f03a38b40c8ba8f3105b3f38e44abd9d882f
CaioRenatoDot <caiorenato146@gmail.com>
2025-05-01 12:27:26 -0300

    feat: fazendo busca por nome e exibindo no html

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 document.getElementById("searchBtn").addEventListener('click', () => {
   const pokemonName = document.getElementById('pokemonInput').value.toLowerCase().trim();
+  const displayDiv = document.getElementById('pokemonDisplay');
 
   if(!pokemonName){
-    console.log('Digite um nome de Pokemon.')
-    return
+    displayDiv.innerHTML = '<p>Digite um nome de Pokémon.</p>';
+    return;
   }
 
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
 
   fetch(url)
-    .then(response =>{
+    .then(response => {
       if(!response.ok){
-        throw new Error('Pokemon não encontrado.')
+        throw new Error('Pokemon não encontrado.');
       }
       return response.json();
     })
-    .then(data =>console.log(data))
-    .catch(error => console.error(error.message));
-    
+    .then(data => {
+      const name = data.name;
+      const sprite = data.sprites.front_default;
+      const types = data.types.map(typeInfo => typeInfo.type.name).join(', ');
+      
+      displayDiv.innerHTML = `
+      <h2>${name.toUpperCase()}</h2>
+      <img src="${sprite}" alt="${name}">
+      <p><strong>Tipo(s):</strong> ${types}</p>
+      `;
+    })
+    .catch(Error => {
+      displayDiv.innerHTML = `<p>${error.message}</p>`;
+    });
 });
